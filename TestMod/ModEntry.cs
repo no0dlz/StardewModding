0a2e0e4bb4201825ef7a0c583a110603e75daa8b
Bastian Schubert <bschubert1990@gmail.com>
2022-03-20 14:28:21 +0100

    .

---
+++ TestMod/ModEntry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using StardewModdingAPI;
+
+namespace Starno0d.TestMod
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            Monitor.Log(helper.Translation.Get("common.starting"), LogLevel.Debug);
+
+            System.Threading.Thread.Sleep(3000);
+
+            Monitor.Log(helper.Translation.Get("common.started"), LogLevel.Debug);
+
+            System.Threading.Thread.Sleep(10000);
+
+
+            /*for (int i = 0; i < 30; i++)
+            {
+                Monitor.Log($"Starno0d TEST MOD LOADED - {i}...", LogLevel.Debug);
+                System.Threading.Thread.Sleep(1000);
+            }*/
+        }
+    }
+}
